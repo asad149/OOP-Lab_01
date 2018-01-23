29917c857c06259eee1c84129ae053c78f173ddc
asad149 <asad.arshad.94849@gmail.com>
2018-01-23 18:37:56 +0500

    OOP Lab_01

---
+++ Factorial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wasiq_Sir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\t\t\t======== Calculation Of Sum And Avergae ======== ");
+            Program p1 = new Program();
+            double a, b;
+            Console.Write("\nEnter First Value : ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("\nEnter Second Value : ");
+            b = double.Parse(Console.ReadLine());
+           double c = p1.sum(a, b);
+ 
+            Console.WriteLine("\nThe Sum  = {0} ",c );
+            Console.WriteLine("\nThe Average = {0} ", c/2);
+
+
+
+
+    Console.ReadLine();
+        }
+        
+        public double sum(double a,double b)
+        {
+
+            double c = a + b;
+            return c;
+
+        }
+    }
+}
---
+++ Sum_Average.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wasiq_Sir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\t\t\t======== Swapping of Numbers ======== ");
+
+            Program p1 = new Program();
+
+            p1.swap();
+    Console.ReadLine();
+        }
+        
+        public void swap()
+        {
+            double a, b;
+            Console.Write("\nEnter First Value : ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("\nEnter Second Value : ");
+            b = double.Parse(Console.ReadLine());
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine("\n\t\t\t======== Values After Swapping ======== ");
+
+
+            Console.WriteLine("\nFirst Value : {0}",a);
+            Console.WriteLine("\nSecond Value : {0} ", b);
+
+            
+        }
+    }
+}
