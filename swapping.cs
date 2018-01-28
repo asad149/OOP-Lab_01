deba8a0e67cef3117ecec744483e110b3b6b189d
asad149 <asad.arshad.94849@gmail.com>
2018-01-28 23:29:47 +0500

    First commit

--- swapping.cs
+++ swapping.cs
@@ -5,38 +5,40 @@ using System.Text;
 
 namespace Wasiq_Sir
 {
+    class vowels
+    {
+       public char[] a = new char[100];
+       public int i, count;
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
 
-            Console.WriteLine("\t\t\t======== Swapping of Numbers ======== ");
+            Console.WriteLine("\t\t\t======== Finding Vowels of Given Name ======== ");
+            vowels v = new vowels();
+            
+            
+            for (v.i = 0; v.i <= 10; v.i++)
+            {
 
-            Program p1 = new Program();
+                v.a[v.i] = Console.ReadKey().KeyChar;
+                
 
-            p1.swap();
+                if (v.a[v.i] == 'a' || v.a[v.i] == 'e' || v.a[v.i] == 'i' || v.a[v.i] == 'o' || v.a[v.i] == 'u')
+                {
+                    v.count++;
+                }
+            }
+            Console.WriteLine("Vowels Present in Given Name" +v.count);
+
+
+          
     Console.ReadLine();
 
         }
         
-        public void swap()
-        {
-            double a, b;
-            Console.Write("\nEnter First Value : ");
-            a = double.Parse(Console.ReadLine());
-            Console.Write("\nEnter Second Value : ");
-            b = double.Parse(Console.ReadLine());
-            a = a + b;
-            b = a - b;
-            a = a - b;
-
-            Console.WriteLine("\n\t\t\t======== Values After Swapping ======== ");
-
-
-            Console.WriteLine("\nFirst Value : {0}",a);
-            Console.WriteLine("\nSecond Value : {0} ", b);
-
-            
-        }
+        
     }
 }
